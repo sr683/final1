c6df1cb4f881c5459ab94d8c33c86ed68b27e84f
shafiur <sr683@njit.edu>
2018-11-26 22:33:31 -0500

    questions added

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -28,9 +28,10 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question;
+        $edit = FALSE;
+        return view('questionForm', ['question' => $question,'edit' => $edit  ]);
     }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -39,8 +40,20 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+        ]);
+        $input = request()->all();
+        $question = new Question($input);
+        $question->user()->associate(Auth::user());
+        $question->save();
+        return redirect()->route('home')->with('message', 'IT WORKS!');
+        // return redirect()->route('questions.show', ['id' => $question->id]);
     }
+
 
     /**
      * Display the specified resource.
