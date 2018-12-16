edc9116980a921c23a6f1927cb39b6b29a0f1635
shafiur <sr683@njit.edu>
2018-12-15 23:13:14 -0500

    Captcha package installed

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-27 02:08:08.
+    * Generated on 2018-12-16 04:05:52.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
